8056e59cd988d21f989b51169594a21d95dcc43e
Erik Axelsson <erikaxelsson1@gmail.com>
2014-03-20 11:55:42 +0100

    cors

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,36 @@
 package controllers;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import models.GitHandler;
 import models.TempFile;
+import play.libs.F.Promise;
 import play.libs.Json;
+import play.libs.Jsonp;
+import play.mvc.Action;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Http.Context;
 import play.mvc.Result;
+import play.mvc.SimpleResult;
 
 public class Application extends Controller {
 	
 	public static Result index(){
-		return ok("this is just the index, you should be looking somewhere else");
+	    final Map<String, String[]> post = request().body().asFormUrlEncoded();
+        //String callback=post.get("callback")[0];
+        String test="asds";
+	    JsonNode jsonObject =Json.toJson(test);
+	   // return new ActionWrapper(super.onRequest(request, actionMethod));
+	    response().setHeader("Access-Control-Allow-Origin", "*");
+	    return ok(jsonObject);
+        //return ok(Jsonp.jsonp(callback, mapper));
+		
+	//	return ok("this is just the index, you should be looking somewhere else");
 	}
 
 	public static Result initRepository() {
@@ -37,6 +57,13 @@ public class Application extends Controller {
 	public static Result getFile(String url) {
 		TempFile tf = GitHandler.getFile(url);
 		return ok(Json.toJson(tf));
+	}
+	public static Result checkPreFlight() {
+	    response().setHeader("Access-Control-Allow-Origin", "*");       // Need to add the correct domain in here!!
+	    response().setHeader("Access-Control-Allow-Methods", "POST");   // Only allow POST
+	    response().setHeader("Access-Control-Max-Age", "300");          // Cache response for 5 minutes
+	    response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");         // Ensure this header is also allowed!  
+	    return ok();
 	}
 
 }
