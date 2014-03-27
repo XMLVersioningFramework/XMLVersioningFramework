b0e506f61aec0c434287f58891edb7a2c4998e4b
Sergio <mail@se.rg.io>
2014-03-27 20:26:59 +0100

    UserHandler.java: adds new line at EOF

--- app/models/UserHandler.java
+++ app/models/UserHandler.java
@@ -1,21 +1,22 @@
 package models;
 
 public class UserHandler {
-	
-	public static String getUserName(int i){
-		String[]  userArray = new String[3];
+
+	public static String getUserName(int i) {
+		String[] userArray = new String[3];
 		userArray[0] = "Alice";
 		userArray[1] = "Bob";
 		userArray[2] = "Charly";
-		
+
 		return userArray[i];
 	}
-	public static String getUserEmail(int i){
-		String[]  userArray = new String[3];
+
+	public static String getUserEmail(int i) {
+		String[] userArray = new String[3];
 		userArray[0] = "Alice@company.com";
 		userArray[1] = "Bob@company.com";
 		userArray[2] = "Charly@company.com";
-		
+
 		return userArray[i];
 	}
 }
